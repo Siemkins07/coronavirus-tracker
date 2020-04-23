09beb9bae8e7efb865d99d239b56ed6ac4497b00
Jacek Pobiarżyn <jacek.pobiarzyn@execon.pl>
2020-04-23 14:00:48 +0200

    improve map loop

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,23 +8,27 @@ import covid from '../../images/covid.png'
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
  
   const variants = [{
-    state: 'Infected',
+    id: 1,
+    name: 'Infected',
+    style: styles.infected,
     end: confirmed,
     text: ' active cases of '
   },
-  {
-    state: 'Recovered',
+    {
+    id: 2,
+    name: 'Recovered',
+    style: styles.recovered,
     end: recovered,
     text: ' recovered cases of '
   },
-  {
-    state: 'Deaths',
+    {
+    id: 3,
+    name: '.deaths',
+    style: styles.deaths,
     end: deaths,
     text: ' deaths caused by '
-  },
-  ];
+  }];
    
-
   if (!confirmed) {
     return (
     <>
@@ -33,23 +37,23 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
       </>
     )}
 
-  return ( 
+  return (
     <div className={styles.container}>
       <Grid container spacing={3} justify='center'>
 
         {variants.map(variant => (
-          <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+          <Grid item component={Card} xs={12} md={3} key={variant.id} className={cx(styles.card, `${variant.style}`)} >
           <CardContent>
-              <Typography color='textSecondary' gutterBottom>{variant.state}</Typography>
+              <Typography color='textSecondary' gutterBottom>{variant.name}</Typography>
             <Typography variant='h4'>
               <CountUp
                 start={0}
                 end={variant.end.value}
-                duration={2.0}
+                duration={3.0}
                 separator=','
               />            
             </Typography>
-            <Typography color='textSecondary' gutterBottom>{new Date(lastUpdate).toLocaleString()}</Typography>
+            <Typography color='textSecondary' gutterBottom>{new Date(lastUpdate).toLocaleDateString()}</Typography>
               <Typography variant="body2">Number of{variant.text}Covid-19</Typography>
           </CardContent>
         </Grid> 
