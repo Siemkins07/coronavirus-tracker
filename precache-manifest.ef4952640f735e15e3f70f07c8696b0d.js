d2d47e4b924f1561a663642f5b5796c03cf2c0a1
Jacek Pobiarżyn <jacek.pobiarzyn@gmial.com>
2020-04-26 11:32:24 +0200

    Updates

---
+++ precache-manifest.ef4952640f735e15e3f70f07c8696b0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618988689f341c762d125015e92d19d1",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "66b0fe0b8a1805e1b0dd",
+    "url": "/coronavirus-tracker/static/css/main.763ff067.chunk.css"
+  },
+  {
+    "revision": "52678bbaa2eeb57e66aa",
+    "url": "/coronavirus-tracker/static/js/2.8c95e75f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus-tracker/static/js/2.8c95e75f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b0fe0b8a1805e1b0dd",
+    "url": "/coronavirus-tracker/static/js/main.7e28614b.chunk.js"
+  },
+  {
+    "revision": "ba2b5318f3996a398754",
+    "url": "/coronavirus-tracker/static/js/runtime-main.56e68336.js"
+  },
+  {
+    "revision": "befdb884cc3d7b84a8a7dd705df07de8",
+    "url": "/coronavirus-tracker/static/media/covid.befdb884.png"
+  },
+  {
+    "revision": "a6c66ccdf37a1d2c8a7fcb9ca1450822",
+    "url": "/coronavirus-tracker/static/media/covid_logo.a6c66ccd.png"
+  }
+]);
